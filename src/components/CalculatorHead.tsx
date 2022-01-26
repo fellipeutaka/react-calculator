aa6f12a294f74c15aceb56ca0aa484621653ad57
fellipeutaka <fellipeutaka@gmail.com>
2022-01-26 14:31:18 -0300

    Header

--- src/components/CalculatorHead.tsx
+++ src/components/CalculatorHead.tsx
@@ -1,7 +1,7 @@
 export default function CalculatorHead() {
   return (
     <header className="w-72 h-36 flex justify-end items-end pb-4 pr-5">
-      <span className="text-white text-5xl">0</span>
+      <span className="text-white text-7xl font-light">0</span>
     </header>
   );
 }
