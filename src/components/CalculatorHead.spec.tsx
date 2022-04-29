035af1137f9248bab949510c7e39f51c4f924ea2
fellipeutaka <fellipeutaka@gmail.com>
2022-04-29 11:58:12 -0300

    * feat(*): Added Jest;

---
+++ src/components/CalculatorHead.spec.tsx
@@ -0,0 +1,9 @@
+import { render } from "@testing-library/react";
+import CalculatorHead from "./CalculatorHead";
+
+describe("Calculator Head component", () => {
+  it("should render Calculator Head component", () => {
+    const { getByText } = render(<CalculatorHead currentValue="99" />);
+    expect(getByText("99")).toBeInTheDocument();
+  });
+});
